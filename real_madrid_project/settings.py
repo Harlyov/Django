2589abb196da703981c6ddbef03a4beffcf6e979
Harlyov <harlyovv@gmail.com>
2026-04-04 14:43:35 +0300

    updates

--- real_madrid_project/settings.py
+++ real_madrid_project/settings.py
@@ -135,8 +135,7 @@ REST_FRAMEWORK = {
 
 
 CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0')
-CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
-
+CELERY_RESULT_BACKEND = None
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
